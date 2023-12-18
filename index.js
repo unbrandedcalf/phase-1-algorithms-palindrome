00d75256d48021506dc058aba8393db4ebdc2cca
Maverick Vitto <mavvitto@yahoo.com>
2023-12-18 13:52:19 -0700

    phase-1-algorithms-palindrome

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function isPalindrome(str) {
+  str = str.toLowerCase();
+
+  let start = 0
+  let end = str.length - 1
+
+  while (start < end) {
+    if (str[start] !== str[end]) {
+      return false
+    }
+    start++
+    end--
+  }
+
+  return true
 }
 
 /* 
